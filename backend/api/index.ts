33683a3af998f18f8ceb5061bf27565595efef24
shivamprasad1001 <shivamprasad1001@gmail.com>
2025-08-23 18:24:47 +0530

    update index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,4 @@
-import { Request, Response } from "express";
-import app from "../src/app";
-import serverless from "serverless-http";
 
-const handler = serverless(app);
+import app from '../src/app';
 
-export default async function (req: Request, res: Response) {
-  return handler(req, res);
-}
+export default app;
