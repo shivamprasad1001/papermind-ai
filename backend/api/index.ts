f67c8c4b49fac692735cd11f0477473e36a6171a
shivamprasad1001 <shivamprasad1001@gmail.com>
2025-08-23 18:10:22 +0530

    fix updates

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,9 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Request, Response } from "express";
 import app from "../src/app";
 import serverless from "serverless-http";
 
 const handler = serverless(app);
 
-export default async function (req: VercelRequest, res: VercelResponse) {
+export default async function (req: Request, res: Response) {
   return handler(req, res);
 }
