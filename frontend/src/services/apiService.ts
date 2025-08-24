b517fce45df5a1420554d918dd36e2cd976096e6
shivamprasad1001 <shivamprasad1001@gmail.com>
2025-08-24 22:31:47 +0530

    add log to debug API BASE URL

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = (import.meta as any).env.VITE_API_BASE_URL || 'http://localhost:3001';
-
+console.log("API_BASE_URL:", API_BASE_URL);
 const apiClient = axios.create({
   baseURL: `${API_BASE_URL}/api`,
 });
