5923ae7f3f77e4472c0639eb0dd0ba674b40a8e3
Shivam Prasad <161421872+shivamprasad1001@users.noreply.github.com>
2025-08-27 18:01:43 +0530

    remove help-btn
    move help-btn in sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { AppProvider } from './context/AppContext';
 import Layout from './components/Layout';
 import ToastContainer from './components/common/ToastContainer';
-import HelpButton from './components/common/HelpButton';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <div className="min-h-screen bg-[var(--bg-primary)] text-[var(--text-primary)] font-sans">
         <Layout />
         <ToastContainer />
-        <HelpButton />
       </div>
     </AppProvider>
   );
